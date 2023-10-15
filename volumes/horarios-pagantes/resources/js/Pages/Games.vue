567c95eb20a53e2d7df8e224a99206ed4a9121b0
Nicollas Cruz <nicollasacruz@gmail.com>
2023-10-15 14:11:59 -0300

    adicionado jogos na home

--- volumes/horarios-pagantes/resources/js/Pages/Games.vue
+++ volumes/horarios-pagantes/resources/js/Pages/Games.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 import Background from '../../../storage/app/public/img/Background.png';
 import horariosPagantes from '../../../storage/app/public/img/horarios-pagantes.png';
 import FortuneTiger from '../../../storage/app/public/img/tiger.png';
@@ -11,26 +11,26 @@ const backgroundStyle = `background-image: url(${Background})`;
 
 </script>
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: start;
-        align-items: center;
-        margin: 0 auto;
-    }
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    margin: 0 auto;
+}
 
-    .box {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 15px 0 15px 0;
-        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
-        transition: transform 0.2s;
-    }
+.box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 15px 0 15px 0;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+    transition: transform 0.2s;
+}
 
-    .box:hover {
-        transform: scale(1.05);
-    }
+.box:hover {
+    transform: scale(1.05);
+}
 </style>
 
 
@@ -40,20 +40,20 @@ const backgroundStyle = `background-image: url(${Background})`;
     <AuthenticatedLayout>
         <div class="py-12 min-h-screen min-w-screen container" :style="backgroundStyle">
             <div class="mb-10 box"><img class="max-w-xs" :src="horariosPagantes" alt=""></div>
-            <span class="box bg-yellow-300 p-2 text-3xl font-bold text-center mx-auto">DESCUBRA OS HORÁRIOS PAGANTES DOS PRINCIPAIS JOGOS</span>
-            <span class="box p-2 text-2xl text-center text-white mx-auto">Selecione um dos jogos abaixo para descobrir os horários pagantes</span>
+            <span class="box bg-yellow-300 p-2 text-3xl font-bold text-center mx-auto">DESCUBRA OS HORÁRIOS PAGANTES DOS
+                PRINCIPAIS JOGOS</span>
+            <span class="box p-2 text-2xl text-center text-white mx-auto">Selecione um dos jogos abaixo para descobrir os
+                horários pagantes</span>
             <div class="box max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="my-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900"><a href="games/fortune-tiger"></a>Fortune Tiger</div>
-                </div>
-
-                <div class="my-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">Fortune OX</div>
-                </div>
-
-                <div class="my-2 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">Mr. Hallow-Win!</div>
-                </div>
+                <Link :href="route('fortune-tiger')">
+                    <img class="px-2" :src="FortuneTiger" alt="Fortune Tiger">
+                </Link>
+                <Link :href="route('fortune-ox')">
+                    <img class="px-2" :src="FortuneOx" alt="Fortune Tiger">
+                </Link>
+                <Link :href="route('hallow-win')">
+                    <img class="px-2" :src="HallowWin" alt="Fortune Tiger">
+                </Link>
             </div>
         </div>
     </AuthenticatedLayout>
