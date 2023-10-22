9bca67447a663fa5b9fe852e6e681a1665ef05d0
Nicollas Cruz <nicollasacruz@gmail.com>
2023-10-22 01:40:36 -0300

    Ajuste fortune

--- volumes/horarios-pagantes/resources/js/Pages/FortuneOx.vue
+++ volumes/horarios-pagantes/resources/js/Pages/FortuneOx.vue
@@ -5,33 +5,48 @@ import BgFortuneOxMobile from '../../../storage/app/public/img/bg-fortune-ox-mob
 import BoxFortuneOx from '../../../storage/app/public/img/box-horarios-ox.png';
 import BoxFortuneOxMobile from '../../../storage/app/public/img/box-horarios-ox-mobile.png';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 function isMobile() {
   if (screen.width <= 760) {
     return true;
   }
-  else {
-    return false;
-  }
+  return false;
 }
-
+function isPc() {
+  if (!isIpad() && !isMobile()) {
+    return true;
+  }
+  return false;
+}
 function isIpad() {
   if (screen.width > 760 && screen.width < 1000) {
     return true;
   }
-  else {
-    return false;
-  }
+  return false;
 }
 
-defineProps({ horarios: Array, porcentagem: Number })
+defineProps({ horarios: Array, porcentagem: Number });
 
 const mobile = isMobile();
 const ipad = isIpad();
+const pc = isPc();
+
 const fundo = `background-image: url(${BgFortuneOx})`;
 const fundoMobile = `background-image: url(${BgFortuneOxMobile})`;
 const box = `background-image: url(${BoxFortuneOx})`;
 const boxMobile = `background-image: url(${BoxFortuneOxMobile})`;
+
+const mostrarHorarioState = ref(false);
+const carregando = ref(false);
+
+function mostrarHorario() {
+  carregando.value = true;
+  setTimeout(() => {
+    carregando.value = false;
+    mostrarHorarioState.value = true;
+  }, 3000);
+}
 
 </script>
 <style>
@@ -43,7 +58,6 @@ body {
 }
 
 .container {
-  /* min-height: 100vh !important; */
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -55,67 +69,43 @@ body {
 }
 
 .containerMobile {
-  /* min-height: 100vh !important; */
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: 100wv;
-  max-height: 127vw;
+  width: 100vw;
+  height: 127vw;
   background-repeat: no-repeat;
   background-size: 100vw auto;
   background-position: start;
 }
 
 .containerIpad {
-  /* min-height: 100vh !important; */
-  width: 100wv;
-  height: 970px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100vw;
+  height: 127vw;
   background-repeat: no-repeat;
   background-size: 100vw auto;
   background-position: start;
 }
 
-.sub-div {
-  margin-top: -20px;
-}
-
-.sub-divIpad {
-  margin-top: 50px;
-}
-
-.sub-div1 {
-  height: 10vw;
-  width: 100vw;
-}
-
-.sub-div1Mobile {
-  height: 30vh;
-  width: 100%;
-}
-
-.sub-div1Ipad {
-  height: 54vh;
-  width: 100%;
-}
-
 .sub-div2 {
-  min-height: 48vh;
   width: 100vw;
+  height: 40%;
   max-width: 1920px !important;
-  background-size: 95vh auto;
+  background-size: auto 100%;
   background-repeat: no-repeat;
   background-position: center;
   display: flex;
-  flex-direction: column;
   justify-content: space-between;
   align-items: center;
 }
 
 .sub-div2Mobile {
   width: 100vw;
-  height: 230px;
-  margin: 0 auto;
-  background-size: 320px;
+  height: 45%;
+  background-size: auto 80%;
   background-repeat: no-repeat;
   background-position: center;
   display: flex;
@@ -125,9 +115,9 @@ body {
 
 .sub-div2Ipad {
   width: 100vw;
-  height: 350px;
+  height: 45%;
   margin: 0 auto;
-  background-size: auto 350px;
+  background-size: auto 80%;
   background-repeat: no-repeat;
   background-position: center;
   display: flex;
@@ -139,29 +129,50 @@ body {
 Fortune Tiger: #AB161B
 Fortune OX: #AB161B */
 
+.botaoPc {
+  width: 30vw;
+  text-align: center;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  font-weight: bold;
+  border-radius: 5px;
+  box-shadow: 0 3px;
+  color: #AB161B;
+  background: linear-gradient(180deg, rgba(253, 184, 39, 1) 0%, rgba(253, 226, 103, 1) 100%);
+}
+
+.botao {
+  width: 40vw;
+  text-align: center;
+  font-family: 'Yanone Kaffeesatz', sans-serif;
+  font-weight: bold;
+  border-radius: 5px;
+  box-shadow: 0 3px;
+  color: #AB161B;
+  background: linear-gradient(180deg, rgba(253, 184, 39, 1) 0%, rgba(253, 226, 103, 1) 100%);
+}
+
 .horarios {
   width: 10vw;
   text-align: center;
   font-family: 'Yanone Kaffeesatz', sans-serif;
   font-weight: bold;
   font-size: 1.7rem;
-  color: #AB161B;
   border-radius: 5px;
   box-shadow: 0 3px;
-  /* background: rgb(253,184,39); */
+  color: #AB161B;
   background: linear-gradient(180deg, rgba(253, 184, 39, 1) 0%, rgba(253, 226, 103, 1) 100%);
 }
 
 .horariosMobile {
-  width: 16vw;
+  width: 18vw;
   text-align: center;
   font-family: 'Yanone Kaffeesatz', sans-serif;
   font-weight: bold;
   font-size: 0.7rem;
-  color: #AB161B;
   border-radius: 3px;
   box-shadow: 0 3px;
   /* background: rgb(253,184,39); */
+  color: #AB161B;
   background: linear-gradient(180deg, rgba(253, 184, 39, 1) 0%, rgba(253, 226, 103, 1) 100%);
 }
 
@@ -171,10 +182,10 @@ Fortune OX: #AB161B */
   font-family: 'Yanone Kaffeesatz', sans-serif;
   font-weight: bold;
   font-size: 1.3rem;
-  color: #AB161B;
   border-radius: 3px;
   box-shadow: 0 3px;
   /* background: rgb(253,184,39); */
+  color: #AB161B;
   background: linear-gradient(180deg, rgba(253, 184, 39, 1) 0%, rgba(253, 226, 103, 1) 100%);
 }
 
@@ -204,46 +215,120 @@ Fortune OX: #AB161B */
   <Head title="Fortune Ox" />
 
   <AuthenticatedLayout>
-    <!-- <template #header> -->
-    <!-- <h2 class="font-semibold text-xl text-white leading-tight">Fortune Ox</h2> -->
-    <!-- </template> -->
-
     <div :class="mobile ? 'containerMobile' : ipad ? 'containerIpad' : 'container'"
       :style="mobile || ipad ? fundoMobile : fundo">
       <div :class="mobile ? 'sub-div1Mobile' : ipad ? 'sub-div1Ipad' : 'sub-div1'"></div>
-      <div v-if="!mobile && !ipad" class="sub-div2" :style="box">
-        <div class="w-[40%] h-6 bg-gray-200 rounded-full dark:bg-gray-700 absolute border border-2 border-black">
-          <div class="h-6 bg-green-600 rounded-full dark:bg-green-500 text-center font-bold"
-            style="width: {{porcentagem}}%">{{ porcentagem }}% de acertividade</div>
+      
+      <!--                PC                 -->
+      <div v-if="pc" class="sub-div2 flex flex-col" :style="box">
+        <div v-if="carregando" class="text-center my-auto">
+          <div role="status" class="flex flex-col justify-center justify-items-center items-center">
+            <svg aria-hidden="true"
+              class="inline w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-red-600"
+              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor" />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill" />
+            </svg>
+            <span class="sr-only">Loading...</span>
+            <span class="font-bold text-3xl text-white">Conectando IA ao servidor da plataforma...</span>
+          </div>
         </div>
-        <div class="grid grid-cols-4 gap-3  z-50 max-w-4xl m-auto">
+
+        <div v-if="mostrarHorarioState" class="grid grid-cols-4 gap-2 max-w-4xl m-auto" style="margin-top: 3%;">
           <span class="horarios" v-for="(horario, index) in horarios" :key="index">
             {{ horario }}
           </span>
         </div>
-      </div>
 
-      <div v-if="ipad" class="sub-div2Ipad" :style="boxMobile">
-        <div class="sub-divIpad grid grid-cols-4 gap-3 z-50 max-w-4xl m-auto">
+        <!-- <div v-if="mostrarHorarioState"
+          class="w-[66%] h-3 bg-gray-200 rounded-full dark:bg-gray-700 border border-2 border-black">
+          <div class="h-3 bg-green-600 rounded-full dark:bg-green-500 text-center font-bold text-xs"
+            style="width: {{porcentagem}}%">{{ porcentagem }}% de assertividade...</div>
+        </div> -->
+        <div v-if="!mostrarHorarioState && !carregando"
+          class="transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 m-auto botaoPc text-4xl"><button
+            type="button" @click="mostrarHorario">CARREGAR HORÁRIOS</button>
+        </div>
+      </div>
+      <!--               IPAD                -->
+      <div v-if="ipad" class="sub-div2Ipad flex flex-col" :style="boxMobile">
+        <div v-if="carregando" class="text-center my-auto">
+          <div role="status" class="flex flex-col justify-center justify-items-center items-center">
+            <svg aria-hidden="true"
+              class="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-red-600"
+              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor" />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill" />
+            </svg>
+            <span class="sr-only">Loading...</span>
+            <span class="font-bold text-xl text-white">Conectando IA ao servidor da plataforma...</span>
+          </div>
+        </div>
+
+        <div v-if="mostrarHorarioState" class="grid grid-cols-4 gap-3 z-50 max-w-4xl m-auto" style="margin-top: 13%;">
           <span class="horariosIpad" v-for="(horario, index) in horarios" :key="index">
             {{ horario }}
           </span>
         </div>
-      </div>
 
-      <div v-if="mobile" class="sub-div2Mobile" :style="boxMobile">
-        <div class="sub-div grid grid-cols-4 gap-1 ">
+
+        <div v-if="!mostrarHorarioState && !carregando"
+          class="transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 m-auto botao text-3xl"><button
+            type="button" @click="mostrarHorario">CARREGAR HORÁRIOS</button>
+        </div>
+      </div>
+      <!--              MOBILE               -->
+      <div v-if="mobile" class="sub-div2Mobile flex flex-col" :style="boxMobile">
+        <div v-if="carregando" class="text-center my-auto">
+          <div role="status" class="flex flex-col justify-center justify-items-center items-center">
+            <svg aria-hidden="true"
+              class="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-red-600"
+              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor" />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill" />
+            </svg>
+            <span class="sr-only">Loading...</span>
+            <span class="font-bold text-sm text-white">Conectando IA ao servidor da plataforma...</span>
+          </div>
+        </div>
+
+
+        <div v-if="mostrarHorarioState" class="grid grid-cols-4 gap-1" style="margin-top: -6%;">
           <span class="horariosMobile" v-for="(horario, index) in horarios" :key="index">
             {{ horario }}
           </span>
+        </div>
+
+
+        <!-- <div v-if="mostrarHorarioState"
+          class="w-[66%] h-3 bg-gray-200 rounded-full dark:bg-gray-700 border border-2 border-black">
+          <div class="h-3 bg-green-600 rounded-full dark:bg-green-500 text-center font-bold text-xs"
+            style="width: {{porcentagem}}%">{{ porcentagem }}% de assertividade...</div>
+        </div> -->
+
+        <div v-if="!mostrarHorarioState && !carregando"
+          class="transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-110 m-auto botao text-xl botao"><button
+            type="button" @click="mostrarHorario">CARREGAR HORÁRIOS</button>
         </div>
       </div>
 
     </div>
     <div class="bg-black">
-      <iframe v-if="!mobile && !ipad" class="iframe"
+      <iframe v-if="!mobile && !ipad && mostrarHorarioState" class="iframe"
         src="https://realsbet.com/casino/game/1970229?provider=MrSlotty&btag=CX-48560_421110" frameborder="0"></iframe>
-      <iframe v-if="ipad" class="iframeMobile"
+      <iframe v-if="ipad && mostrarHorarioState" class="iframeMobile"
         src="https://m.realsbet.com/casino/game/1970229?provider=MrSlotty&btag=CX-48560_421110" frameborder="0"></iframe>
       <iframe v-if="mobile" class="iframeMobile"
         src="https://m.realsbet.com/casino/game/1970229?provider=MrSlotty&btag=CX-48560_421110" frameborder="0"></iframe>
