92711b16dcb11cc1fb61b091aadf1d3a81469663
Nicollas Cruz <nicollasacruz@gmail.com>
2023-11-03 08:31:25 -0300

    ajuste ipad

--- volumes/horarios-pagantes/resources/js/Pages/NinjaCrash.vue
+++ volumes/horarios-pagantes/resources/js/Pages/NinjaCrash.vue
@@ -89,7 +89,7 @@ body {
   flex-direction: column;
   justify-content: space-between;
   width: 100vw;
-  height: 127vw;
+  height: 178vw;
   background-repeat: no-repeat;
   background-size: 100vw auto;
   background-position: start;
